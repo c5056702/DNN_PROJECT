103830a35e8efa830de958d2c5f2450b81ea420a
Manikanta Inapakurthi <c5056702@my.shu.ac.uk>
2025-12-28 18:17:26 +0500

    Add sample limit arguments for index preparation
    Added options to limit the number of samples for training, validation, and testing.

--- src/prepare_manifest.py
+++ src/prepare_manifest.py
@@ -27,6 +27,9 @@ def main() -> None:
     parser.add_argument("--val_frac", type=float, default=0.10)
     parser.add_argument("--seed", type=int, default=42)
     parser.add_argument("--k_steps", type=int, default=4)
+    parser.add_argument("--limit_train", type=int, default=None)
+    parser.add_argument("--limit_val", type=int, default=None)
+    parser.add_argument("--limit_test", type=int, default=None)
     parser.add_argument("--out_train", type=Path, default=Path("data/index_train.json"))
     parser.add_argument("--out_val", type=Path, default=Path("data/index_val.json"))
     parser.add_argument("--out_test", type=Path, default=Path("data/index_test.json"))
@@ -42,10 +45,10 @@ def main() -> None:
     train_story_indices = [storyid_to_idx[sid] for sid in train_ids]
     val_story_indices = [storyid_to_idx[sid] for sid in val_ids]
 
-    train_index = build_index_table(train_raw, train_story_indices, K=args.k_steps, limit_samples=None)
-    val_index = build_index_table(train_raw, val_story_indices, K=args.k_steps, limit_samples=None)
+    train_index = build_index_table(train_raw, train_story_indices, K=args.k_steps, limit_samples=args.limit_train)
+    val_index = build_index_table(train_raw, val_story_indices, K=args.k_steps, limit_samples=args.limit_val)
     test_story_indices = list(range(len(test_raw)))
-    test_index = build_index_table(test_raw, test_story_indices, K=args.k_steps, limit_samples=None)
+    test_index = build_index_table(test_raw, test_story_indices, K=args.k_steps, limit_samples=args.limit_test)
 
     save_index(args.out_train, train_index)
     save_index(args.out_val, val_index)
